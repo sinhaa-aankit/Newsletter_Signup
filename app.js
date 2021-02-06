622704b6a315c460a00d3b9ec4c6cee38a0cc1f9
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-02-06 09:53:52 +0530

    Completed core design of Sign Up page. (bootstrap cdn added)

--- app.js
+++ app.js
@@ -3,18 +3,55 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
-const app = express();
+
 const request = require("request");
+
+const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 
 
-
-
-
+	
 
 
 app.listen(3000, function(){
 	console.log("Server running on port 3000");
 });
+
+
+// app.get("/", function(req, res){
+//  res.sendFile(__dirname + "/signup.html");
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
