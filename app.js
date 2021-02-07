7a4232da39bc54bf0ce6aefc48f867751788068f
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-02-07 09:18:14 +0530

    stored required data inside new variable sent through homepage request. (first name, last name, email).

--- app.js
+++ app.js
@@ -17,12 +17,23 @@ app.get("/", function(req, res){
  res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", function(req,res){
+
+	var firstName = req.body.fName;
+	var lastName = req.body.lName;
+	var email = req.body.Email;
+
+	
+
+});
 
 	
 
 app.listen(3000, function(){
 	console.log("Server running on port 3000");
 });
+
+//var apiKey = "3b6e69e4dc72925458d2a79a582290ad-us7";
 
 
 
