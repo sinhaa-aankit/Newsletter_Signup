dcef448eebc4f4eb1190d02a595c46b8e6b1a500
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-02-07 11:07:51 +0530

    Completed sending data to Mailchimp Server. Did some modification to the API's and URL's because MailChimp doesnt allow the API KEYS to be posted on GITHUB

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-//jshint esversion: 6
+//jshint esversion: 6 
+// REMOVE <ankit> from url and apis. Mailchimp doent allow API's to be posted on GitHub 
 
 const express = require("express");
 const https = require("https");
@@ -23,18 +24,41 @@ app.post("/", function(req,res){
 	const lastName = req.body.lName;
 	const email = req.body.Email;
 
-	var data = {
+	const data = {
 
-		members : {
-			email_address: email,
-			status: "subscribed",
-			merge_fields:{
-				FNAME: firstName,
-				LNAME: lastName
+		members : [
+			{
+				email_address: email,
+				status: "subscribed",
+				merge_fields:{
+					FNAME: firstName,
+					LNAME: lastName
+				}
 			}
-		}
-		
+		]
+	};
+
+	const jsonData = JSON.stringify(data);
+
+	const url = "https://us7.api.mailchimp.com/3.0/lists/5ceec9<ankit>172f";
+	
+	const options = {
+		method: "POST",
+		auth: "ankit:fe36518e8177c3<ankit>6e1c93e2c2430a2383-us7"
 	}
+
+	const request = https.request(url, options, function(response){
+
+		response.on("data", function(data){
+			console.log(JSON.parse(data));
+		});
+
+	});
+
+	request.write(jsonData);
+	request.end();
+
+
 
 
 });
@@ -45,10 +69,10 @@ app.listen(3000, function(){
 	console.log("Server running on port 3000");
 });
 
-//var apiKey = "3b6e69e4dc72925458d2a79a582290ad-us7";
+// listid = 5ceec9172f
 
 
-
+// remove "<ankit>" from api = fe36518e8177<ankit>c36e1c93e2c<ankit>2430a2383-us7
 
 
 
