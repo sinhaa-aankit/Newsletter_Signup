a6929d7d952fca95e27e674f0bb2139b5f0f75dc
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-02-07 08:35:48 +0530

    sent back the signup.html file to the homepage through app.js using node.js

--- app.js
+++ app.js
@@ -11,18 +11,17 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-
+app.get("/", function(req, res){
+ res.sendFile(__dirname + "/signup.html");
+});
 	
-
 
 app.listen(3000, function(){
 	console.log("Server running on port 3000");
 });
 
 
-// app.get("/", function(req, res){
-//  res.sendFile(__dirname + "/signup.html");
-// });
+
 
 
 
