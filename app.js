9ac98148134949131931069d395246f9733fd50b
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-02-07 08:50:10 +0530

    Solved static page error. made a static page public and used it through node. moved neccessary resources into public folder

--- app.js
+++ app.js
@@ -10,10 +10,14 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static("public"));
+
 
 app.get("/", function(req, res){
  res.sendFile(__dirname + "/signup.html");
 });
+
+
 	
 
 app.listen(3000, function(){
